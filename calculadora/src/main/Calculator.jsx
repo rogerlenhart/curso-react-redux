9df4692e552d04de5964d465065870feea2d0f8c
Róger Lenhart <rogerlenhart15@gmail.com>
2022-05-28 03:03:07 -0300

    Início do projeto da calculadora.

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import './Calculator.css'
+
+import React from 'react'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+
+            </div>
+        )
+    }
+
+}
