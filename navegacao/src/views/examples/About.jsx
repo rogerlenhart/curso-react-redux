f19da27266c377a33597eeb83d30537b275941da
Róger Lenhart <roger-ti@caiweb.com.br>
2022-05-31 12:47:21 -0300

    Estrutura do projeto com React Router.

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,11 @@
+import './About.css'
+import React from 'react'
+
+const About = props => (
+    <div className='About'>
+        <h1>Sobre</h1>
+        <p>O nosso sistema foi criado...</p>
+    </div>
+)
+
+export default About
