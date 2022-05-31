87dbff5e7459f65a262386efae64b6b8564e2761
Róger Lenhart <roger-ti@caiweb.com.br>
2022-05-31 13:27:55 -0300

    Estilo básico.

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,8 +1,10 @@
+import './index.css'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+
 import App from './views/App'
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(<App/>)
