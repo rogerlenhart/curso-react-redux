c8f86538aca530b57df0b6366b8ee55c32d793d0
Róger Lenhart <rogerlenhart15@gmail.com>
2022-06-11 09:53:58 -0300

    Navegação dinâmica.

--- fundamentos-next/src/pages/index.jsx
+++ fundamentos-next/src/pages/index.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link"
+import Navegador from "../components/Navegador"
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href="/estiloso">
-        Estiloso
-      </Link>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: 'wrap',
+      height: '100vh'
+    }}>
+      <Navegador texto="Estiloso" destino="/estiloso" cor="#3B7075"/>
+      <Navegador texto="Exemplo" destino="/exemplo"/>
+      <Navegador texto="JSX" destino="/jsx" cor="#FC7623"/>
+      <Navegador texto="Navegação #01" destino="/navegacao" cor="#862517" />
+      <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor="#6F797B" />
     </div>
   )
 }
