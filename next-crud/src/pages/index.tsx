2cbe50de6fb6887b119fc3f24ce1630f0ab9a978
Róger Lenhart <rogerlenhart15@gmail.com>
2022-06-16 15:23:56 -0300

    Layout básico.

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-      
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-slate-900 text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
